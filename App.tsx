46bceece145a22d844c18b3700b0ce0a877c1557
HiroNonoyama <h.nonoyama.117@gmail.com>
2022-05-30 10:00:37 +0900

    Add notification

--- App.tsx
+++ App.tsx
@@ -1,22 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import "expo-dev-client";
+import * as Notifications from "expo-notifications";
+import { useEffect, useState } from "react";
+import { Alert, Button, Platform, Text, View } from "react-native";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
-
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
+async function registerForPushNotificationsAsync() {
+  let token;
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+  if (existingStatus !== "granted") {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
   }
+  if (finalStatus !== "granted") {
+    alert("Failed to get push token for push notification!");
+    return;
+  }
+  token = (await Notifications.getExpoPushTokenAsync()).data;
+  console.log(token);
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+  return token;
+}
+
+export default function App2() {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => {
+      if (token) setExpoPushToken(token);
+    });
+  }, []);
+
+  const handlePress = () => {
+    Alert.alert("set notification");
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "title",
+        body: "body",
+        data: {},
+      },
+      trigger: { seconds: 1, channelId: "reminder" },
+    });
+  };
+  const [isInitial, setIsInitial] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setIsInitial(false), 1000);
+  }, []);
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      {isInitial && <Text>Initial</Text>}
+      <Text>{expoPushToken}</Text>
+      <Button title="set reminder" onPress={handlePress} />
+    </View>
+  );
 }
